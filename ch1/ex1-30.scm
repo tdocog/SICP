73f44f8ff22ecf5e3512e68a772551d9c5a8a4df
tdocog <137963355+tdocog@users.noreply.github.com>
2024-10-11 08:39:04 -0500

    Add files via upload

---
+++ ch1/ex1-30.scm
@@ -0,0 +1,9 @@
+
+(define (itersum term a next b) 
+  (define (iter a result) 
+          (if (> a b) 
+              result 
+              (iter (next a) (+ result (term a))))) 
+  (iter a 0)
+) 
+
